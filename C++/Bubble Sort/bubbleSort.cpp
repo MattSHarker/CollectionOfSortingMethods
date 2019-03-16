9a84d0fd7f4adcb6df16e8ab4c9b75f0dcda9591
MattSHarker <mattsharker@gmail.com>
2019-03-16 15:14:38 -0700

    Bubble Sort
    Created Bubble Sort in C++
    Also created a header file to make this file and future ones easier to read

---
+++ C++/Bubble Sort/bubbleSort.cpp
@@ -0,0 +1,54 @@
+
+// Matthew Harker
+// Bubble Sort in C++
+
+#include <iostream>
+#include "sortUtilities.hpp"
+
+using namespace std;
+
+const int SIZE = 8;
+
+
+// an optimized version of bubble sort
+void bubbleSort(int* arr, int size)
+{
+    bool swapped = false;   // if nothing swaps then list is sorted
+
+    for (int i = 0; i < size; ++i)
+    {
+        swapped = false;
+
+        for (int j = 0; j < size - i - 1; j++)
+        {
+            if (arr[j] > arr[j+1])
+            {
+                swap (&arr[j], &arr[j+1]);
+                swapped = true;
+            }
+        }
+
+        if (!swapped)
+            break;
+    }
+}
+
+
+int main()
+{
+    cout << "Bubble Sort" << endl;
+
+    int arr[SIZE];
+    initArr(arr, SIZE);
+
+    cout << "Original Array: ";
+    printArr(arr, SIZE);
+
+    bubbleSort(arr, SIZE);
+
+    cout << "Sorted Array:   ";
+    printArr(arr, SIZE);
+
+    return 0;
+}
+
